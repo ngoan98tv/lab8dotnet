5977f61afd3b393dbc90be17a8a3f6e8cf727c77
Ngoan Tran <ngoan98travinh@gmail.com>
2020-04-12 14:01:31 +0700

    refresh data table on update point success

--- lab8/Home.cs
+++ lab8/Home.cs
@@ -13,6 +13,7 @@ namespace lab8
     public partial class Home : Form
     {
         private string teacher_id;
+        private string current_subject_id;
         public Home(string id, string name)
         {
             InitializeComponent();
@@ -50,26 +51,23 @@ namespace lab8
             }
         }
 
-        private void Course_Click(object sender, EventArgs args)
+        private void Load_Students_Table(string subject_id)
         {
             stateLabel.Text = "Đang tải...";
-            Button btn = sender as Button;
-            string subject_id = btn.Name;
-            // Clear all data and refresh.
             dataTable.DataSource = null;
             dataTable.Rows.Clear();
             dataTable.Columns.Clear();
             dataTable.Refresh();
-            
+
             string query = "SELECT s.id as 'MSSV', s.lastname as 'Họ', s.firstname as 'Tên', " +
                 "l.name as 'Lớp', CONCAT(c.subject_id, '-', c.id) as 'Mã HP', " +
                 "p.value as 'Điểm' " +
-                "FROM student AS s " + 
+                "FROM student AS s " +
                 "JOIN course AS c ON c.class_id = s.class_id " +
                 "JOIN class AS l ON l.id = c.class_id " +
                 "LEFT JOIN point AS p ON (p.student_id = s.id AND p.course_id = c.id) " +
                 "WHERE c.subject_id = '" + subject_id + "';";
-            
+
             DataTable data = Database.Instance.LoadData(query);
             dataTable.DataSource = data;
 
@@ -81,6 +79,14 @@ namespace lab8
             dataTable.Columns.Add(actionsCol);
             this.dataTable.Visible = true;
             stateLabel.Text = "Đã tải thành công " + data.Rows.Count.ToString() + " sinh viên.";
+        }
+
+        private void Course_Click(object sender, EventArgs args)
+        {
+            Button btn = sender as Button;
+            string subject_id = btn.Name;
+            current_subject_id = subject_id;
+            Load_Students_Table(subject_id);
         }
 
         private void backButton_Click(object sender, EventArgs e)
@@ -108,8 +114,14 @@ namespace lab8
                 string rPoint = row.Cells[pointIndex].Value.ToString();
 
                 UpdatePoint updatePoint = new UpdatePoint(rMSSV, rFirstname, rLastname, rCourse, rPoint);
+                updatePoint.Disposed += new EventHandler(Point_FormClosed);
                 updatePoint.Visible = true;
             }
+        }
+
+        private void Point_FormClosed(object sender, EventArgs e)
+        {
+            Load_Students_Table(current_subject_id);
         }
 
         private void Home_FormClosed(object sender, FormClosedEventArgs e)
